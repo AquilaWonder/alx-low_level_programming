2d52a9782a98769f6b387c0f7cd30540299a628c
AquilaWonder <aquilaedudzi@gmail.com>
2023-03-24 22:37:37 -0700

    trailing space removed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,15 +4,17 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
+
 void print_triangle(int size)
 {
 	if (size <= 0)
 	{
 		_putchar('\n');
-	} else
+	}
+	else
 	{
 		int i, j;
-		
+
 		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
